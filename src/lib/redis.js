8cc3350f3d0ec97d3d84c95b69a35d71d97954c6
Jardelmc <jardel.casteluber@gmail.com>
2020-04-08 23:44:02 -0300

    fix - add security for redis

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -2,6 +2,10 @@ import redis from 'redis';
 
 const client = redis.createClient();
 
+if (process.env.NODE_ENV === 'production') {
+  client.AUTH(process.env.REDIS_AUTH);
+}
+
 client.on('error', err => {
   console.log('Erro ao iniciar Redis ', err);
 });
